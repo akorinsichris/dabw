8f565b91d3563845234627bb9bb31db893115e05
akorinsichris <104876344+akorinsichris@users.noreply.github.com>
2023-07-05 15:12:52 +0800

    Update sf_test.py

--- sf_test.py
+++ sf_test.py
@@ -5,3 +5,18 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('Employee Details')
+
+conn = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+# execute a query
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM resource_data")
+results = cursor.fetchall()
+
+# Displat query results
+for row in results:
+  st.write(row)
+
+# Close Snowflake connection
+cursor.close()
+conn.close()
